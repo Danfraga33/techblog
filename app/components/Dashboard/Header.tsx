ab29e78e2aa7e62100384d713efecbfd8c4890f6
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-23 10:11:25 +1100

    chore: make breadcrumb path dynamic

--- app/components/Dashboard/Header.tsx
+++ app/components/Dashboard/Header.tsx
@@ -9,8 +9,11 @@ import {
 } from "../ui/breadcrumb";
 import { Separator } from "../ui/separator";
 import { SidebarTrigger } from "../ui/sidebar";
+import { getPathName } from "~/lib/utils";
+import { useLocation } from "@remix-run/react";
 
 const Header = () => {
+  const path = getPathName(useLocation().pathname) as string;
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
@@ -18,14 +21,9 @@ const Header = () => {
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb>
           <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
             <BreadcrumbSeparator className="hidden md:block" />
             <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              <BreadcrumbPage>{path}</BreadcrumbPage>
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
