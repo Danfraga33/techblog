7d02b190884455fb418c4bdc1a7eade45ff74702
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-24 08:45:15 +1100

    feat: add podcast player component

---
+++ app/components/PodcastPlayer.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import {
+  ChevronLeft,
+  MoreVertical,
+  Star,
+  Shuffle,
+  SkipBack,
+  Pause,
+  SkipForward,
+  Repeat,
+} from "lucide-react";
+import { Separator } from "./ui/separator";
+import FadedDivider from "./Dashboard/StyleComponents.tsx/FadedDivider";
+import { Link } from "@remix-run/react";
+import Header from "./Dashboard/Header";
+
+type podcastType = {
+  id: number;
+  title: string;
+  description: string;
+  pubDate: string;
+  category: string;
+  href: string;
+  slug: string;
+  slogan: string;
+};
+export default function PodcastPlayer({
+  selectedPodcast,
+}: {
+  selectedPodcast: podcastType;
+}) {
+  return (
+    <>
+      <Header />
+      <div className="min-h-screen bg-gradient-to-b from-[#3d2329] to-[#1a1314] text-white">
+        <div className="px-4 py-6">
+          <div className="mb-8 flex items-center justify-between">
+            <button className="rounded-full bg-slate-200/10 p-2 transition hover:bg-white/10">
+              <ChevronLeft className="h-6 w-6 opacity-100" />
+            </button>
+
+            <div>
+              <h1 className="text-lg font-medium">{selectedPodcast.title}</h1>
+            </div>
+            <button className="rounded-full p-2 transition hover:bg-white/10">
+              <MoreVertical className="h-6 w-6" />
+            </button>
+          </div>
+
+          <div className="mb-12 text-center">
+            <h2 className="mb-8 px-8 text-2xl font-bold md:text-3xl">
+              {selectedPodcast.slogan}
+            </h2>
+
+            {/* Add image fade here */}
+            <div className="relative mb-12 inline-block">
+              <img
+                src="/chip.jpg"
+                alt="Podcast host"
+                width={600}
+                height={600}
+                className="toopacity-30 mx-auto w-4/5 rounded-lg opacity-65"
+              />
+            </div>
+
+            {/* Waveform */}
+            <div className="mb-4 h-12">
+              <FadedDivider />
+            </div>
+
+            {/* Time */}
+            <div className="mb-4 text-sm text-gray-400">00:23/02:34</div>
+
+            {/* Controls */}
+            <div className="mb-8 flex items-center justify-evenly gap-8">
+              <button className="rounded-full p-2 transition hover:bg-white/10">
+                <Shuffle className="h-10 w-10 text-gray-400" />
+              </button>
+              <Link
+                to="/podcasts"
+                className="rounded-full border border-gray-500/100 p-2 transition hover:bg-white/10"
+              >
+                <SkipBack className="h-10 w-10" />
+              </Link>
+              <button className="rounded-full bg-white p-3 transition hover:bg-white/90">
+                <Pause className="h-6 w-6 text-black" />
+              </button>
+              <button className="rounded-full border border-gray-500/100 p-2 transition hover:bg-white/10">
+                <SkipForward className="h-10 w-10" />
+              </button>
+              <button className="rounded-full p-2 transition hover:bg-white/10">
+                <Repeat className="h-10 w-10 text-gray-400" />
+              </button>
+            </div>
+
+            {/* Caption */}
+            <div className="text-sm text-gray-400">
+              <p>{selectedPodcast.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
