fa0aaf46125d9ee3a412934b6da000aba1f5d388
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-24 12:08:25 +1100

    refactor: add resources data

---
+++ app/data/resources.ts
@@ -0,0 +1,12 @@
+export const resources = [
+  {
+    name: "CB Inishgts",
+    link: "https://www.cbinsights.com/",
+    category: "AI",
+  },
+  {
+    name: "Semiconductor Engineering",
+    link: "https://semiengineering.com/about-us/",
+    category: "Semiconductor",
+  },
+];
