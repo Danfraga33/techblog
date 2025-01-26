ac22fb9764d210a934488ce196b9c4174e5e9989
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-26 17:21:18 +1100

    chore: add link to sidemenu logo button

--- app/components/team-switcher.tsx
+++ app/components/team-switcher.tsx
@@ -5,6 +5,7 @@ import {
   SidebarMenuButton,
 } from "~/components/ui/sidebar";
 import { Separator } from "./ui/separator";
+import { Link } from "@remix-run/react";
 
 export function TeamSwitcher({
   teams,
@@ -20,12 +21,14 @@ export function TeamSwitcher({
   return (
     <SidebarMenu>
       <SidebarMenuItem className="flex justify-center py-2">
-        <SidebarMenuButton
-          size="lg"
-          className="flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-sidebar-primary text-sidebar-primary-foreground transition-all"
-        >
-          <activeTeam.logo className="h-8 w-8" />
-        </SidebarMenuButton>
+        <Link to="/">
+          <SidebarMenuButton
+            size="lg"
+            className="flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-sidebar-primary text-sidebar-primary-foreground transition-all"
+          >
+            <activeTeam.logo className="h-8 w-8" />
+          </SidebarMenuButton>
+        </Link>
       </SidebarMenuItem>
       <Separator />
     </SidebarMenu>
