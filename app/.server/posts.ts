68834d1a98e70198a771abf1f30d780761a550d7
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-28 19:50:21 +1100

    feat: add mdx blog functions

--- app/.server/posts.ts
+++ app/.server/posts.ts
@@ -1,23 +1,42 @@
-// app/utils/getPosts.server.ts
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import crypto from "crypto";
 
-// import type { PostMeta } from "~/types";
+type Frontmatter = {
+  title: string;
+  description: string;
+  coverImage: string;
+  tags: string[];
+  toc: string;
+  date: string;
+  id?: string;
+};
 
 const postsDirectory = path.join(process.cwd(), "app/content/posts");
 
 export const getPosts = async () => {
-  const postsDirectory = path.join(process.cwd(), "app/content/posts");
   const filenames = fs.readdirSync(postsDirectory);
 
   const posts = filenames.map((filename) => {
     const filePath = path.join(postsDirectory, filename);
     const fileContent = fs.readFileSync(filePath, "utf8");
-    const { data: frontmatter } = matter(fileContent);
+    const { data } = matter(fileContent);
+    const frontmatter = data as Frontmatter;
     const slug = filename.replace(/\.mdx$/, "");
 
-    return { slug, frontmatter };
+    const id = crypto
+      .createHash("md5")
+      .update(filename + frontmatter.title + frontmatter.date)
+      .digest("hex");
+
+    return {
+      slug,
+      frontmatter: {
+        ...frontmatter,
+        id,
+      },
+    };
   });
 
   // Sort posts by date (newest first)
