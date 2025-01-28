d4d3582cb8f766da025d4426a1d89e00ec643582
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-28 19:50:10 +1100

    feat: add mdx podcasts functions

---
+++ app/.server/podcasts.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import crypto from "crypto";
+export type PodcastFrontmatter = {
+  title: string;
+  description: string;
+  date: string;
+  category: string;
+  slogan: string;
+  coverImage: string;
+  audioFile: string;
+  id?: string;
+};
+export const getPodcasts = async () => {
+  const postsDirectory = path.join(process.cwd(), "app/content/podcasts");
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const posts = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    const { data } = matter(fileContent);
+    const frontmatter = data as PodcastFrontmatter;
+    const slug = filename.replace(/\.mdx$/, "");
+
+    const id = crypto
+      .createHash("md5")
+      .update(filename + frontmatter.title + frontmatter.date)
+      .digest("hex");
+
+    return {
+      slug,
+      frontmatter: {
+        ...frontmatter,
+        id,
+      },
+    };
+  });
+
+  const sortedPosts = posts.sort(
+    (a, b) =>
+      new Date(b.frontmatter.date).getTime() -
+      new Date(a.frontmatter.date).getTime(),
+  );
+  return sortedPosts;
+};
