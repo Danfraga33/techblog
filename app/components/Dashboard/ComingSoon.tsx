5a24404ccadec9083a2bdcff10fa35424bbb1123
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-31 20:56:46 +1100

    style: add larger gap in coming soon

--- app/components/Dashboard/ComingSoon.tsx
+++ app/components/Dashboard/ComingSoon.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ComingSoon = () => {
   return (
-    <h1 className="flex scroll-m-20 items-center gap-2 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="flex scroll-m-20 items-center gap-4 text-4xl font-extrabold tracking-tight lg:text-5xl">
       Coming soon...
       <Timer />
     </h1>
