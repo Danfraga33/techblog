92a4dc8f3e656aa66de702f82efc2de066ee244b
Daniel Fraga <dan.fraga33@gmail.com>
2025-02-24 13:42:57 +1100

    fix: deactive signup

--- app/components/Dashboard/SmallSignup.tsx
+++ app/components/Dashboard/SmallSignup.tsx
@@ -10,23 +10,23 @@ export const SmallSignup = () => {
         title="Blog"
         description=" Insightful analysis on emerging technology, exploring AI, quantum computing, semiconductors, and industry advancements."
       />
-      <Form method="post" className="flex flex-col gap-2">
-        <div className="flex max-w-md gap-2">
-          <Input
-            type="email"
-            placeholder="Enter your email"
-            className="rounded-full border-secondary-50 shadow-md"
-            name="email"
-          />
-          <Button
-            type="submit"
-            variant="default"
-            className="rounded-full px-6 text-muted shadow-md"
-          >
-            Subscribe
-          </Button>
-        </div>
-      </Form>
+
+      <div className="flex max-w-md gap-2">
+        <Input
+          type="email"
+          placeholder="Enter your email"
+          className="rounded-full border-secondary-50 shadow-md"
+          name="email"
+        />
+        <Button
+          type="submit"
+          variant="default"
+          className="rounded-full px-6 text-muted shadow-md"
+          onClick={() => console.log("Subscribed")}
+        >
+          Subscribe
+        </Button>
+      </div>
     </>
   );
 };
