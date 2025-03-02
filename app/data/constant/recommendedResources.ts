2ec2597eb93a0af0ad2585f73c394893f00bc483
Daniel Fraga <dan.fraga33@gmail.com>
2025-03-02 14:51:22 +1100

    fix: add cover pictures to recommended resources

--- app/data/constant/recommendedResources.ts
+++ app/data/constant/recommendedResources.ts
@@ -2,13 +2,31 @@ export const resources = [
   {
     name: "CB Insihgts",
     link: "https://www.cbinsights.com/",
-    avatarImg: "/chip.jpg",
+    avatarImg: "/CB_Insights_Logo.png",
     category: "AI",
   },
   {
     name: "Semiconductor Engineering",
     link: "https://semiengineering.com/about-us/",
     category: "Semiconductor",
-    avatarImg: "/chip.jpg",
+    avatarImg: "/Semicondutor_Engineeering_Logo.jpg",
+  },
+  {
+    name: "EE Times - AI Big Data",
+    link: "https://www.eetimes.com/designline/ai-big-data-designline/",
+    category: "AI",
+    avatarImg: "/EE_Times_Logo.jpg",
+  },
+  {
+    name: "IEEE Spectrum (Semiconductor)",
+    link: "https://spectrum.ieee.org/topic/semiconductors/",
+    category: "Semiconductor",
+    avatarImg: "/ieee.png",
+  },
+  {
+    name: "Next Platform",
+    link: "https://www.nextplatform.com/",
+    category: "Semiconductor",
+    avatarImg: "/Next_Platform_Logo.jpg",
   },
 ];
