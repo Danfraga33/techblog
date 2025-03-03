85fc14eeb589ea165b20e620d8849aecbe8d7caa
Daniel Fraga <dan.fraga33@gmail.com>
2025-03-03 19:13:06 +1100

    feat: add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,29 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicon/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon/favicon-16x16.png",
+  },
+  {
+    rel: "icon",
+    type: "image/x-icon",
+    href: "/favicon/favicon.ico",
+  },
+  { rel: "manifest", href: "/favicon/site.webmanifest" },
 ];
 
 export function Layout({ children }: { children: ReactNode }) {
