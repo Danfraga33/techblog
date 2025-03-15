83453a980054fbaea35b77e70ac2012e2a16c17e
Daniel Fraga <dan.fraga33@gmail.com>
2025-03-16 10:21:24 +1100

    feat: add header

--- app/components/Dashboard/Header.tsx
+++ app/components/Dashboard/Header.tsx
@@ -1,59 +1,43 @@
 import { Link } from "@remix-run/react";
-import { Fragment, useState } from "react";
-import { SmallSignup } from "./SmallSignup";
-import { Crown, Menu, Paperclip, Plus, Search, X } from "lucide-react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-} from "@heroui/navbar";
-import { Button } from "../ui/button";
+import { TwitterIcon } from "lucide-react";
+import UnderlineAnimation from "./UnderlineAnimation";
 
-function NavItem({ label }: { label: string }) {
+export default function Header() {
   return (
-    <div className="group relative">
-      <Button
-        variant="secondary"
-        className="flex items-center rounded-2xl bg-white px-4 py-0.5 text-sm font-medium text-gray-700 hover:text-gray-900"
-      >
-        {label === "NextFrontier" ? <Crown /> : ""}
-        {label}
-        {label === "NextFrontier" ? "" : <Plus />}
-      </Button>
-    </div>
-  );
-}
+    <header className="border-b border-black py-6">
+      <div className="container mx-auto flex items-center justify-between px-4">
+        <Link
+          to="/"
+          className="font-heading text-2xl font-bold uppercase text-black"
+        >
+          Next Frontier Hub
+        </Link>
 
-const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const menuItems = [
-    "NextFrontier",
-    "Articles",
-    "Podcast",
-    "Newsletters",
-    "Contact",
-  ];
-
-  return (
-    <header className="sticky top-0 z-10 bg-transparent">
-      <div className="">
-        <div className="flex items-center space-x-4">
-          <nav className="hidden items-center space-x-1 md:flex">
-            {menuItems.map((item, i) => (
-              <Fragment key={i}>
-                <NavItem label={item} />
-              </Fragment>
-            ))}
+        <div className="flex items-center gap-8">
+          <nav className="text-md hidden gap-8 font-semibold uppercase tracking-tighter md:flex">
+            <UnderlineAnimation>
+              <Link to="/newsletter">Newsletter</Link>
+            </UnderlineAnimation>
+            <UnderlineAnimation>
+              <Link to="/blog">blog</Link>
+            </UnderlineAnimation>
+            <UnderlineAnimation>
+              <Link to="/podcasts">Podcasts</Link>
+            </UnderlineAnimation>
           </nav>
+
+          <Link
+            to="https://x.com/Danfraga33"
+            aria-label="Twitter"
+            className="pl-10"
+          >
+            <TwitterIcon
+              className="text-black hover:text-primary hover:transition-all"
+              size={20}
+            />
+          </Link>
         </div>
       </div>
     </header>
   );
-};
-
-export default Header;
+}
