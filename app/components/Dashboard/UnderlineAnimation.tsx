673f2ed716f5a9b6525468ccdc40b735df317c58
Daniel Fraga <dan.fraga33@gmail.com>
2025-03-16 10:23:12 +1100

    style: add underlineanimation component

---
+++ app/components/Dashboard/UnderlineAnimation.tsx
@@ -0,0 +1,20 @@
+import React, { Fragment, PropsWithChildren, ReactNode } from "react";
+import { cn } from "~/lib/utils";
+
+interface UnderlineAnimationProps extends PropsWithChildren {
+  className?: string;
+}
+
+const UnderlineAnimation = ({
+  children,
+  className,
+}: UnderlineAnimationProps) => {
+  return (
+    <div className={cn("group relative overflow-hidden text-black", className)}>
+      {children}
+      <span className="absolute bottom-0 left-0 h-[2px] w-full translate-x-[-100%] bg-primary transition-transform duration-300 group-hover:translate-x-0"></span>
+    </div>
+  );
+};
+
+export default UnderlineAnimation;
