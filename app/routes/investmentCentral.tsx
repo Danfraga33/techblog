58d553307cd1fff1de5831a96a1dfb75192bbbdc
Daniel Fraga <dan.fraga33@gmail.com>
2025-03-18 22:25:19 +1100

    feat: add new WIP routes

---
+++ app/routes/investmentCentral.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InvestmentCentral = () => {
+  return (
+    <>
+      <h1>Anaylsis page according to the Framework</h1>
+      <ol>
+        <li>Trends</li>
+        <li>Ideas</li>
+        <li>Companies</li>
+      </ol>
+    </>
+  );
+};
+
+export default InvestmentCentral;
--- app/routes/whiteboardAnimation.tsx
+++ app/routes/whiteboardAnimation.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import ComingSoon from "~/components/Dashboard/ComingSoon";
-import ContentLayout from "~/components/Dashboard/ContentLayout";
 
 const whiteboardAnimation = () => {
-  return (
-    <>
-      <ContentLayout
-        title="Whiteboard Animations"
-        description="Engaging visual storytelling simplifies AI, quantum computing, semiconductors, and emerging technology concepts effectively."
-      >
-        <ComingSoon />
-      </ContentLayout>
-    </>
-  );
+  return <>In Progress</>;
 };
 
 export default whiteboardAnimation;
