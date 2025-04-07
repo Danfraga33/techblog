3cb5565cda4ac890592a4eed43a64154715c6a8f
Daniel Fraga <dan.fraga33@gmail.com>
2025-04-08 09:18:04 +1000

    fix: hide members menu option

--- app/data/constant/header.ts
+++ app/data/constant/header.ts
@@ -2,5 +2,4 @@ export const menuItems = [
   { title: "Newletter", link: "/Newsletter" },
   { title: "Blog", link: "/blog" },
   { title: "Podcasts", link: "/podcasts" },
-  { title: "Member", link: "/member" },
 ];
